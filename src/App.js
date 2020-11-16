340c09d34481a6f08b0429bb5d84fbbd2d551cf1
Priest <desoyeolayinka@gmail.com>
2020-11-16 17:23:58 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Navbar from './components/navbar.component'
 
 
 function App() {
   return (
+
     <React.Fragment>
-    
-    <div> gfb </div>
-    
+        <Router> 
+
+          <Navbar/>
+   
+        </Router>
+
+
     
     </React.Fragment>
   );
